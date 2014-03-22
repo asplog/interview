bfc8fc710ec5339fa9f4e212f161ea224ee90fbc
kotan.kn <kotan.kn@gmail.com>
2014-03-22 14:12:53 +0900

    okay

--- Quatre/A1.c
+++ Quatre/A1.c
@@ -18,7 +18,7 @@ int node_get_balanced_height(const node_t*node,int depth)
 	{
 		int l=node_get_balanced_height(node->gauche,depth+1);
 		int r=node_get_balanced_height(node->droite,depth+1);
-		depth=0<l&&0<r&&abs(l-r)<2?max(l,r):-1;
+		depth=0<l&&0<r&&(l-r)-(((l-r)<<1)&((l-r)>>31))<2?r+(l-r)&((~(l-r)^(l^r)&((l-r)^l))>>31):-1;
 #ifdef __DEBUG__
 		fprintf(stderr,"%d(%d) %d %d(%d) is %d\n",node->gauche?node->gauche->value:-1,l,node->value,node->droite?node->droite->value:-1,r,depth);	
 #endif
@@ -27,10 +27,8 @@ int node_get_balanced_height(const node_t*node,int depth)
 }
 int main()
 {
-	int depth;
-	node_t*tree=0;
-	if(1)
 	{
+		node_t*tree=0;
 		bst_add(&tree,3);
 		bst_add(&tree,1);
 		bst_add(&tree,4);
@@ -39,17 +37,20 @@ int main()
 		bst_add(&tree,9);
 		bst_add(&tree,2);
 		bst_add(&tree,6);
+		fprintf(stdout,"Tree is %s\n",0<node_get_balanced_height(tree,0)?"balanced":"not balanced");
+		node_linked_free(tree);
 	}
-	else
+//	else
 	{
+		node_t*tree=0;
 		bst_add(&tree,4);
 		bst_add(&tree,2);
 		bst_add(&tree,6);
 		bst_add(&tree,1);
 		bst_add(&tree,3);
 		bst_add(&tree,5);
+		fprintf(stdout,"Tree is %s\n",0<node_get_balanced_height(tree,0)?"balanced":"not balanced");
+		node_linked_free(tree);
 	}
-	depth=node_get_balanced_height(tree,0);
-	fprintf(stdout,"Tree is %s\n",0<depth?"balanced":"not balanced");
 	return 0;
 }
--- Quatre/A3.c
+++ Quatre/A3.c
@@ -18,7 +18,7 @@ int node_get_balanced_height(const node_t*node,int depth)
 	{
 		int l=node_get_balanced_height(node->gauche,depth+1);
 		int r=node_get_balanced_height(node->droite,depth+1);
-		depth=0<l&&0<r&&abs(l-r)<2?max(l,r):-1;
+		depth=0<l&&0<r&&(l-r)-(((l-r)<<1)&((l-r)>>31))<2?max(l,r):-1;
 #ifdef __DEBUG__
 		fprintf(stderr,"%d(%d) %d %d(%d) is %d\n",node->gauche?node->gauche->value:-1,l,node->value,node->droite?node->droite->value:-1,r,depth);	
 #endif
@@ -39,26 +39,13 @@ void gen_short_tree(node_t**tree,const int*array,unsigned int length)
 		K/=2;
 	}
 }
-void gen_minimal_tree(node_t**,const int*array,unsigned int length)
-{
-	{
-		unsigned int k,K;
-		node_t*l=0;
-		node_t*p=0;
-		node_t*r=0;
-		for(k=0,K=length;k<K;++k)
-		{
-			node_t*node=node_new(array[k]);
-		}
-	}
-}
 int main()
 {
 	int depth;
 	node_t*tree=0;
 	int values[]={2,3,5,7,11,13,17,19,23,29,31,37,41,47,53,59,61,67,71,73,79,89,91,93,97};
-	//gen_short_tree(&tree,values,(sizeof values)/(sizeof*values));
-	gen_minimal_tree(&tree,values,(sizeof values)/(sizeof*values),0);
+	gen_short_tree(&tree,values,(sizeof values)/(sizeof*values));
+	//gen_minimal_tree(&tree,values,(sizeof values)/(sizeof*values),0);
 	depth=node_get_balanced_height(tree,0);
 	fprintf(stdout,"Tree is %s\n",0<depth?"balanced":"not balanced");
 	node_linked_search(tree,dump,0);
